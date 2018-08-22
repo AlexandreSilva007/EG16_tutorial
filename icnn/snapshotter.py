a552cfb032f20de0f224fcee68ea2d8907f17e4b
AlexandreSilva007 <39566599+AlexandreSilva007@users.noreply.github.com>
2018-08-22 15:25:53 -0400

    Update snapshotter.py

--- icnn/snapshotter.py
+++ icnn/snapshotter.py
@@ -33,31 +33,32 @@ class Snapshotter(object):
 
         self.db[k] = [v_]
         print(self.db[k])
-        #filename = k+"/pmat.zip"
-        #print(filename)
-        #os.makedirs(os.path.dirname(filename), exist_ok=True)
-
-        #output_file = open(filename, "wb+")
-        #pickle.dump([v_], output_file)
-        #output_file.close()
         
-        #auth.authenticate_user()
-        #gauth = GoogleAuth()
-        #gauth.credentials = GoogleCredentials.get_application_default()
-        #drive = GoogleDrive(gauth)
-        #file1 = drive.CreateFile({'title': 'mat.zip'})
-        #file1.SetContentFile(filename)
-        #file1.Upload()
-        #files.download(filename)
+        filename = k+"/pmat.zip"
+        print(filename)
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+        output_file = open(filename, "wb+")
+        pickle.dump([v_], output_file)
+        output_file.close()
+        
+        auth.authenticate_user()
+        gauth = GoogleAuth()
+        gauth.credentials = GoogleCredentials.get_application_default()
+        drive = GoogleDrive(gauth)
+        file1 = drive.CreateFile({'title': 'mat.zip'})
+        file1.SetContentFile(filename)
+        file1.Upload()
+        files.download(filename)
                 
-        #filename = k+"/mat.zip"
-        #os.makedirs(os.path.dirname(filename), exist_ok=True)
+        filename = k+"/mat.zip"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
             
-        #newFile = open(k+"/mat.zip", "wb+")
-        #newFileByteArray = bytes([v_])
-        #newFile.write(newFileByteArray)
-        #newFile.close()
-        #files.download(k+"/mat.zip")
+        newFile = open(k+"/mat.zip", "wb+")
+        newFileByteArray = bytes([v_])
+        newFile.write(newFileByteArray)
+        newFile.close()
+        files.download(k+"/mat.zip")
 
     def load(self, k):
         return pickle.loads(zlib.decompress(self.db[k][:][0].tostring()))
